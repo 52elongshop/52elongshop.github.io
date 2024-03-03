4857104135714a164ba298a1c00769259bb095eb
flydragonle <elongshop@outlook.com>
2024-03-03 19:32:08 +0800

    Site updated: 2024-03-03 19:32:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/02/28/hello-world/","2020/12/01/My-New-PostMB/"];function toRandomPost(){
+var posts=["2020/12/01/My-New-PostMB/","2024/02/28/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
