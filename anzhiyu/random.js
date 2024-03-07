8fcf978d70c1ecd0f8333cd03b737fa47f18dba9
flydragonle <elongshop@outlook.com>
2024-03-07 22:34:13 +0800

    Site updated: 2024-03-07 22:34:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2020/12/01/My-New-PostMB/","2024/03/03/GitHub上优秀的嵌入式开源项目/","2024/02/28/hello-world/"];function toRandomPost(){
+var posts=["2024/03/03/GitHub上优秀的嵌入式开源项目/","2020/12/01/My-New-PostMB/","2024/02/28/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
